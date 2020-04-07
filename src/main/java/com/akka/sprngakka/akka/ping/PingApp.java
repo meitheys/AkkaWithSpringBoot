252cd5dcaf98a76330ab4e8523bd0ccd096ddc75
Matheus Furtado <50843672+meitheys@users.noreply.github.com>
2020-04-07 14:29:57 -0300

    [BUGFIX] Spring done!

--- src/main/java/com/akka/sprngakka/akka/ping/PingApp.java
+++ src/main/java/com/akka/sprngakka/akka/ping/PingApp.java
@@ -4,19 +4,32 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import com.akka.sprngakka.akka.message.Mensagem;
+import com.akka.sprngakka.akka.spring.Extender;
 import com.typesafe.config.ConfigFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import javax.annotation.PostConstruct;
 
 @EnableAutoConfiguration
 @SpringBootApplication
 public class PingApp {
 
+	@Autowired
+	private ApplicationContext context;
 	public static void main(String[] args) {
+		System.getProperties().put( "server.port", 6666);
+		SpringApplication.run(PingApp.class, args);
+	}
 
+	@PostConstruct
+	void iniciarPing() {
 			// Criação de um Actor System, container Akka.
 			ActorSystem system = ActorSystem.create("RemotePing", ConfigFactory.load().getConfig("RemotePing"));
-
+			Extender.getInstance().get(system).initialize(context);
 			// Criando o ator PING
 			ActorRef actorRef = system.actorOf(Props.create(AtorPing.class));
 
--- src/main/java/com/akka/sprngakka/akka/pong/PongApp.java
+++ src/main/java/com/akka/sprngakka/akka/pong/PongApp.java
@@ -2,22 +2,40 @@ package com.akka.sprngakka.akka.pong;
 
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import com.akka.sprngakka.akka.ping.PingApp;
+import com.akka.sprngakka.akka.spring.Extender;
+import com.akka.sprngakka.akka.spring.Propriedades;
+import com.akka.sprngakka.akka.supervisor.SupervisorPong;
 import com.typesafe.config.ConfigFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.annotation.PostConstruct;
 
 @EnableAutoConfiguration
 @SpringBootApplication
 public class PongApp {
 
+	@Autowired
+	private ApplicationContext context;
 	public static void main(String[] args) {
+		System.getProperties().put( "server.port", 5150);
+		SpringApplication.run(PongApp.class, args);
+	}
 
+
+	@PostConstruct
+	void iniciarPong() {
 		//Criando um Actor System
 		ActorSystem system = ActorSystem.create("RemotePong", ConfigFactory.load().getConfig("RemotePong"));
-
+		Extender.getInstance().get(system).initialize(context);
 		//Criando ator PONG
+//		system.actorOf(Propriedades.create(system, SupervisorPong.class), "AtorPong");
 		system.actorOf(Props.create(AtorPong.class), "AtorPong");
-
 		system.getWhenTerminated();
 	}
 }
